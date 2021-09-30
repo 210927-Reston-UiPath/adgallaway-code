494130ec54c9df6d84218dfca5d55e5d19ed1adc
adgallaway <aaron@gallaway.us>
2021-09-30 15:11:52 -0400

    first commit

---
+++ phoneBookApp/IMenu.vb
@@ -0,0 +1,4 @@
+Interface IMenu
+    Sub Start()
+End Interface
+#210297-Reston-UiPath/adgallaway-code
---
+++ phoneBookApp/MainMenu.vb
@@ -0,0 +1,21 @@
+Class MainMenu
+    Implements IMenu
+    sub Initialize() Implements Imenu.Start
+     Console.WriteLine("Hello World!")
+        dim repeat as Boolean = true
+        Do While repeat
+            Console.WriteLine("What would you like to do?")
+            Console.WriteLine("[0] Say hello")
+            Console.WriteLine("[x] Exit")
+            dim input as string = Console.ReadLine()
+            Select Case input
+                Case "0"
+                    Console.WriteLine("Hello")
+                Case "x"
+                    Console.WriteLine("Goodbye")
+                    repeat = false
+            End Select
+        Loop
+    End Sub
+End Class
+#210297-Reston-UiPath/adgallaway-code
