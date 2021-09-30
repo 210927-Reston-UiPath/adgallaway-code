f33dc8638114691c8b276f901651f7c8769544a5
adgallaway <aaron@gallaway.us>
2021-09-30 18:54:28 -0400

    Major Update to phoneBookApp

---
+++ phoneBookApp/Contact.vb
@@ -0,0 +1,17 @@
+Class Contact
+   Property Name As string
+   Property PhoneNumber As Integer 
+   Public Sub New(ByVal name as string, ByVal number as Integer)
+        Me.Name = name
+        Me.PhoneNumber = number
+    End sub
+    Public Sub New()
+        '* parameterless constructor
+    End Sub
+
+
+    Overrides Function ToString() As string
+        return $"Name: {Me.Name} Number: {Me.PhoneNumber}"
+    End Function
+
+End Class
