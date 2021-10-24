9f4b7435008d6020203e7997b088d271dd14c46c
adgallaway <aaron@gallaway.us>
2021-10-24 13:35:43 -0400

    updated html

--- HTML-Stuff/LittleRhodyStore/index.js
+++ HTML-Stuff/LittleRhodyStore/index.js
@@ -16,11 +16,24 @@ function buy(name) {
     let itemQuantity = newItem.insertCell(2);
     itemQuantity.innerHTML = document.querySelector(elementId + ' td .quantity').value;
 
+    let subTotal = newItem.insertCell(3);
+    subTotal.innerHTML = '$' + itemPrice.innerHTML.replace("$","") * itemQuantity.innerHTML;
+
     //resets the input value so the quantity of the original order doesn't "stick"
     document.querySelector(elementId + ' td .quantity').value = '';
+
+    //calculate the total
+    let total = 0;
+    document.querySelectorAll('#cart tbody tr').forEach(row => { 
+        //convert the subtotal to an int using parse int because it is currently being read as a string
+        total = total + parseInt(row.cells[3].innerHTML.replace('$',''))
+    });
+    document.querySelector('#total').innerHTML = '$' + total;
 }
 function checkout()
 {
     document.querySelectorAll('#cart tbody tr').forEach(row => row.remove());
     alert("thanks for buyin")
+
+    total = 0;
 }
